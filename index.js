d8844b9419339738ba2e615050fda26585b5b4f7
Stephen Brady <stephenbrady365@gmail.com>
2024-03-02 17:34:12 +0000

    Basic working version

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const dropZones = document.querySelectorAll(".drop-zone");
 dropZones.forEach((dropZone) => {
   dropZone.addEventListener("dragenter", (event) => event.preventDefault());
   dropZone.addEventListener("dragover", (event) => event.preventDefault());
-  dropZone.addEventListener("drop", (event) => event.preventDefault());
+  dropZone.addEventListener("drop", (event) => {
+    event.preventDefault();
+    event.currentTarget.appendChild(dragged);
+  });
 });
 
 const boxes = document.querySelectorAll(".box");
