ba6f409f750f20b449be00818e686151c406bfcc
Stephen <stephenbrady365@gmail.com>
2024-03-04 14:13:11 +0000

    add ability to rearrange elements

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 let dragged = null;
+let dropPoint = null;
+const BOX_HEIGHT = 50;
 
 const dropZones = document.querySelectorAll(".drop-zone");
 
 dropZones.forEach((dropZone) => {
   dropZone.addEventListener("dragenter", (event) => event.preventDefault());
+
   dropZone.addEventListener("dragover", (event) => event.preventDefault());
+
   dropZone.addEventListener("drop", (event) => {
     event.preventDefault();
-    event.currentTarget.appendChild(dragged);
+    const dropZone = event.currentTarget;
+    if (dropZone.hasChildNodes()) {
+      const childNodes = Array.from(dropZone.childNodes).filter((node) => {
+        return (
+          node.nodeType === Node.ELEMENT_NODE &&
+          node.nodeName === "DIV" &&
+          node.classList.contains("box")
+        );
+      });
+
+      let insertBeforeNodeAtIndex = Math.floor(event.offsetY / BOX_HEIGHT);
+
+      if (insertBeforeNodeAtIndex > childNodes.length) {
+        dropZone.appendChild(dragged);
+      } else {
+        dropZone.insertBefore(dragged, childNodes[insertBeforeNodeAtIndex]);
+      }
+    } else {
+      dropZone.appendChild(dragged);
+    }
   });
 });
 
